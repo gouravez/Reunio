2161db37f8fbe1014c5619a7a4c1c022b7606af9
Gourav <junejagourav2006@gmail.com>
2026-04-14 20:13:41 +0530

    Refactor CORS configuration to use allowed origins for enhanced security

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,17 +4,29 @@ const cors = require("cors");
 const app = express();
 const cookieParser = require("cookie-parser");
 
-const corsOptions = {
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-};
-
 const { errorHandler } = require("./middlewares/errorHandler.middleware");
 const healthRouter = require("./routes/health.route");
 const authRouter = require("./routes/auth.route");
 const sessionRouter = require("./routes/session.route");
 
-app.use(cors(corsOptions));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://reunio-live.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed"));
+      }
+    },
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
